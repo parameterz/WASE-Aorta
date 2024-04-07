27b6b5e7e7f04870c0c5c9a744b62e4ec725d3bb
Dan Dyar <dan@parameterz.com>
2024-04-07 16:54:54 -0700

    graphs for the generic male vs female are working

--- dataModel.js
+++ dataModel.js
@@ -6,116 +6,116 @@ const bsaArray = [
   1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4,
 ];
 
-const xAxisLabels = {
-  "bsa indexed": bsaArray,
-  "height indexed": htArray,
-  "height^2.13 indexed": htArray,
+const indexType = {
+  bsa: "bsa indexed",
+  ht: "height indexed",
+  ht213: "height^2.13 indexed",
 };
 
 const models = {
   male: {
     generic: {
       AOV: {
-        "bsa indexed": { mean: 11.3, sd: 1.3 },
-        "height indexed": { mean: 12.3, sd: 1.2 },
-        "height^2.13 indexed": { mean: 6.6, sd: 0.8 },
+        "bsa indexed": { mean: 11.3, sd: 1.3, xAxis: bsaArray },
+        "height indexed": { mean: 12.3, sd: 1.2, xAxis: htArray },
+        "height^2.13 indexed": { mean: 6.6, sd: 0.8, xAxis: htArray },
       },
       SOV: {
-        "bsa indexed": { mean: 17.2, sd: 2.5 },
-        "height indexed": { mean: 18.6, sd: 2.3 },
-        "height^2.13 indexed": { mean: 10.1, sd: 1.5 },
+        "bsa indexed": { mean: 17.2, sd: 2.5, xAxis: bsaArray },
+        "height indexed": { mean: 18.6, sd: 2.3, xAxis: htArray },
+        "height^2.13 indexed": { mean: 10.1, sd: 1.5, xAxis: htArray },
       },
       STJ: {
-        "bsa indexed": { mean: 14.8, sd: 2.3 },
-        "height indexed": { mean: 16.0, sd: 2.2 },
-        "height^2.13 indexed": { mean: 8.7, sd: 1.4 },
+        "bsa indexed": { mean: 14.8, sd: 2.3, xAxis: bsaArray },
+        "height indexed": { mean: 16.0, sd: 2.2, xAxis: htArray },
+        "height^2.13 indexed": { mean: 8.7, sd: 1.4, xAxis: htArray },
       },
     },
     race: {
       Asian: {
         AOV: {
-          "bsa indexed": { mean: 11.8, sd: 1.3 },
-          "height indexed": { mean: 12.4, sd: 1.2 },
+          "bsa indexed": { mean: 11.8, sd: 1.3, xAxis: bsaArray },
+          "height indexed": { mean: 12.4, sd: 1.2, xAxis: htArray },
         },
         SOV: {
-          "bsa indexed": { mean: 18.2, sd: 2.5 },
-          "height indexed": { mean: 19.1, sd: 2.2 },
+          "bsa indexed": { mean: 18.2, sd: 2.5, xAxis: bsaArray },
+          "height indexed": { mean: 19.1, sd: 2.2, xAxis: htArray },
         },
         STJ: {
-          "bsa indexed": { mean: 15.6, sd: 2.2 },
-          "height indexed": { mean: 16.3, sd: 2.1 },
+          "bsa indexed": { mean: 15.6, sd: 2.2, xAxis: bsaArray },
+          "height indexed": { mean: 16.3, sd: 2.1, xAxis: htArray },
         },
       },
       Black: {
         AOV: {
-          "bsa indexed": { mean: 10.8, sd: 1.2 },
-          "height indexed": { mean: 11.9, sd: 1.1 },
+          "bsa indexed": { mean: 10.8, sd: 1.2, xAxis: bsaArray },
+          "height indexed": { mean: 11.9, sd: 1.1, xAxis: htArray },
         },
         SOV: {
-          "bsa indexed": { mean: 15.8, sd: 2.0 },
-          "height indexed": { mean: 17.4, sd: 1.7 },
+          "bsa indexed": { mean: 15.8, sd: 2.0, xAxis: bsaArray },
+          "height indexed": { mean: 17.4, sd: 1.7, xAxis: htArray },
         },
         STJ: {
-          "bsa indexed": { mean: 13.4, sd: 1.9 },
-          "height indexed": { mean: 14.8, sd: 1.7 },
+          "bsa indexed": { mean: 13.4, sd: 1.9, xAxis: bsaArray },
+          "height indexed": { mean: 14.8, sd: 1.7, xAxis: htArray },
         },
       },
       White: {
         AOV: {
-          "bsa indexed": { mean: 11.4, sd: 1.3 },
-          "height indexed": { mean: 11.9, sd: 1.0 },
+          "bsa indexed": { mean: 11.4, sd: 1.3, xAxis: bsaArray },
+          "height indexed": { mean: 11.9, sd: 1.0, xAxis: htArray },
         },
         SOV: {
-          "bsa indexed": { mean: 17.9, sd: 2.4 },
-          "height indexed": { mean: 18.7, sd: 2.1 },
+          "bsa indexed": { mean: 17.9, sd: 2.4, xAxis: bsaArray },
+          "height indexed": { mean: 18.7, sd: 2.1, xAxis: htArray },
         },
         STJ: {
-          "bsa indexed": { mean: 15.7, sd: 2.3 },
-          "height indexed": { mean: 16.4, sd: 2.0 },
+          "bsa indexed": { mean: 15.7, sd: 2.3, xAxis: bsaArray },
+          "height indexed": { mean: 16.4, sd: 2.0, xAxis: htArray },
         },
       },
     },
     age: {
       "18-40": {
         AOV: {
-          "bsa indexed": { mean: 11.3, sd: 1.3 },
-          "height indexed": { mean: 12.3, sd: 1.1 },
+          "bsa indexed": { mean: 11.3, sd: 1.3, xAxis: bsaArray },
+          "height indexed": { mean: 12.3, sd: 1.1, xAxis: htArray },
         },
         SOV: {
-          "bsa indexed": { mean: 16.5, sd: 2.2 },
-          "height indexed": { mean: 17.8, sd: 2.0 },
+          "bsa indexed": { mean: 16.5, sd: 2.2, xAxis: bsaArray },
+          "height indexed": { mean: 17.8, sd: 2.0, xAxis: htArray },
         },
         STJ: {
-          "bsa indexed": { mean: 14.1, sd: 2.0 },
-          "height indexed": { mean: 15.2, sd: 1.9 },
+          "bsa indexed": { mean: 14.1, sd: 2.0, xAxis: bsaArray },
+          "height indexed": { mean: 15.2, sd: 1.9, xAxis: htArray },
         },
       },
       "41-65": {
         AOV: {
-          "bsa indexed": { mean: 11.2, sd: 1.4 },
-          "height indexed": { mean: 12.3, sd: 1.2 },
+          "bsa indexed": { mean: 11.2, sd: 1.4, xAxis: bsaArray },
+          "height indexed": { mean: 12.3, sd: 1.2, xAxis: htArray },
         },
         SOV: {
-          "bsa indexed": { mean: 17.4, sd: 2.4 },
-          "height indexed": { mean: 19.0, sd: 2.1 },
+          "bsa indexed": { mean: 17.4, sd: 2.4, xAxis: bsaArray },
+          "height indexed": { mean: 19.0, sd: 2.1, xAxis: htArray },
         },
         STJ: {
-          "bsa indexed": { mean: 15.0, sd: 2.2 },
-          "height indexed": { mean: 16.5, sd: 2.1 },
+          "bsa indexed": { mean: 15.0, sd: 2.2, xAxis: bsaArray },
+          "height indexed": { mean: 16.5, sd: 2.1, xAxis: htArray },
         },
       },
       ">65": {
         AOV: {
-          "bsa indexed": { mean: 11.3, sd: 1.4 },
-          "height indexed": { mean: 12.2, sd: 1.2 },
+          "bsa indexed": { mean: 11.3, sd: 1.4, xAxis: bsaArray },
+          "height indexed": { mean: 12.2, sd: 1.2, xAxis: htArray },
         },
         SOV: {
-          "bsa indexed": { mean: 18.2, sd: 2.7 },
-          "height indexed": { mean: 19.5, sd: 2.4 },
+          "bsa indexed": { mean: 18.2, sd: 2.7, xAxis: bsaArray },
+          "height indexed": { mean: 19.5, sd: 2.4, xAxis: htArray },
         },
         STJ: {
-          "bsa indexed": { mean: 15.7, sd: 2.6 },
-          "height indexed": { mean: 16.9, sd: 2.4 },
+          "bsa indexed": { mean: 15.7, sd: 2.6, xAxis: bsaArray },
+          "height indexed": { mean: 16.9, sd: 2.4, xAxis: htArray },
         },
       },
     },
@@ -123,157 +123,179 @@ const models = {
   female: {
     generic: {
       AOV: {
-        "bsa indexed": { mean: 11.9, sd: 1.4 },
-        "height indexed": { mean: 12.2, sd: 1.3 },
-        "height^2.13 indexed": { mean: 7.2, sd: 0.9 },
+        "bsa indexed": { mean: 11.9, sd: 1.4, xAxis: bsaArray },
+        "height indexed": { mean: 12.2, sd: 1.3, xAxis: htArray },
+        "height^2.13 indexed": { mean: 7.2, sd: 0.9, xAxis: htArray },
       },
       SOV: {
-        "bsa indexed": { mean: 18.0, sd: 2.6 },
-        "height indexed": { mean: 18.4, sd: 2.3 },
-        "height^2.13 indexed": { mean: 10.8, sd: 1.6 },
+        "bsa indexed": { mean: 18.0, sd: 2.6, xAxis: bsaArray },
+        "height indexed": { mean: 18.4, sd: 2.3, xAxis: htArray },
+        "height^2.13 indexed": { mean: 10.8, sd: 1.6, xAxis: htArray },
       },
       STJ: {
-        "bsa indexed": { mean: 15.6, sd: 2.4 },
-        "height indexed": { mean: 15.9, sd: 2.2 },
-        "height^2.13 indexed": { mean: 9.4, sd: 1.5 },
+        "bsa indexed": { mean: 15.6, sd: 2.4, xAxis: bsaArray },
+        "height indexed": { mean: 15.9, sd: 2.2, xAxis: htArray },
+        "height^2.13 indexed": { mean: 9.4, sd: 1.5, xAxis: htArray },
       },
     },
     race: {
       Asian: {
         AOV: {
-          "bsa indexed": { mean: 12.2, sd: 1.5 },
-          "height indexed": { mean: 12.1, sd: 1.3 },
+          "bsa indexed": { mean: 12.2, sd: 1.5, xAxis: bsaArray },
+          "height indexed": { mean: 12.1, sd: 1.3, xAxis: htArray },
         },
         SOV: {
-          "bsa indexed": { mean: 18.7, sd: 2.9 },
-          "height indexed": { mean: 18.6, sd: 2.6 },
+          "bsa indexed": { mean: 18.7, sd: 2.9, xAxis: bsaArray },
+          "height indexed": { mean: 18.6, sd: 2.6, xAxis: htArray },
         },
         STJ: {
-          "bsa indexed": { mean: 16.2, sd: 2.8 },
-          "height indexed": { mean: 16.1, sd: 2.6 },
+          "bsa indexed": { mean: 16.2, sd: 2.8, xAxis: bsaArray },
+          "height indexed": { mean: 16.1, sd: 2.6, xAxis: htArray },
         },
       },
       Black: {
         AOV: {
-          "bsa indexed": { mean: 11.8, sd: 1.5 },
-          "height indexed": { mean: 12.7, sd: 1.2 },
+          "bsa indexed": { mean: 11.8, sd: 1.5, xAxis: bsaArray },
+          "height indexed": { mean: 12.7, sd: 1.2, xAxis: htArray },
         },
         SOV: {
-          "bsa indexed": { mean: 17.1, sd: 2.5 },
-          "height indexed": { mean: 18.3, sd: 2.1 },
+          "bsa indexed": { mean: 17.1, sd: 2.5, xAxis: bsaArray },
+          "height indexed": { mean: 18.3, sd: 2.1, xAxis: htArray },
         },
         STJ: {
-          "bsa indexed": { mean: 14.7, sd: 2.2 },
-          "height indexed": { mean: 15.8, sd: 2.0 },
+          "bsa indexed": { mean: 14.7, sd: 2.2, xAxis: bsaArray },
+          "height indexed": { mean: 15.8, sd: 2.0, xAxis: htArray },
         },
       },
       White: {
         AOV: {
-          "bsa indexed": { mean: 11.3, sd: 1.3 },
-          "height indexed": { mean: 11.8, sd: 1.3 },
+          "bsa indexed": { mean: 11.3, sd: 1.3, xAxis: bsaArray },
+          "height indexed": { mean: 11.8, sd: 1.3, xAxis: htArray },
         },
         SOV: {
-          "bsa indexed": { mean: 17.3, sd: 2.7 },
-          "height indexed": { mean: 18.1, sd: 2.5 },
+          "bsa indexed": { mean: 17.3, sd: 2.7, xAxis: bsaArray },
+          "height indexed": { mean: 18.1, sd: 2.5, xAxis: htArray },
         },
         STJ: {
-          "bsa indexed": { mean: 15.1, sd: 2.3 },
-          "height indexed": { mean: 15.8, sd: 2.0 },
+          "bsa indexed": { mean: 15.1, sd: 2.3, xAxis: bsaArray },
+          "height indexed": { mean: 15.8, sd: 2.0, xAxis: htArray },
         },
       },
     },
     age: {
       "18-40": {
         AOV: {
-          "bsa indexed": { mean: 12.0, sd: 1.4 },
-          "height indexed": { mean: 12.1, sd: 1.2 },
+          "bsa indexed": { mean: 12.0, sd: 1.4, xAxis: bsaArray },
+          "height indexed": { mean: 12.1, sd: 1.2, xAxis: htArray },
         },
         SOV: {
-          "bsa indexed": { mean: 17.3, sd: 2.4 },
-          "height indexed": { mean: 17.4, sd: 2.3 },
+          "bsa indexed": { mean: 17.3, sd: 2.4, xAxis: bsaArray },
+          "height indexed": { mean: 17.4, sd: 2.3, xAxis: htArray },
         },
         STJ: {
-          "bsa indexed": { mean: 15.0, sd: 2.2 },
-          "height indexed": { mean: 15.2, sd: 2.0 },
+          "bsa indexed": { mean: 15.0, sd: 2.2, xAxis: bsaArray },
+          "height indexed": { mean: 15.2, sd: 2.0, xAxis: htArray },
         },
       },
     },
     "41-65": {
       AOV: {
-        "bsa indexed": { mean: 11.7, sd: 1.4 },
-        "height indexed": { mean: 12.2, sd: 1.3 },
+        "bsa indexed": { mean: 11.7, sd: 1.4, xAxis: bsaArray },
+        "height indexed": { mean: 12.2, sd: 1.3, xAxis: htArray },
       },
       SOV: {
-        "bsa indexed": { mean: 17.9, sd: 2.4 },
-        "height indexed": { mean: 18.6, sd: 2.0 },
+        "bsa indexed": { mean: 17.9, sd: 2.4, xAxis: bsaArray },
+        "height indexed": { mean: 18.6, sd: 2.0, xAxis: htArray },
       },
       STJ: {
-        "bsa indexed": { mean: 15.6, sd: 2.2 },
-        "height indexed": { mean: 16.2, sd: 2.0 },
+        "bsa indexed": { mean: 15.6, sd: 2.2, xAxis: bsaArray },
+        "height indexed": { mean: 16.2, sd: 2.0, xAxis: htArray },
       },
     },
     ">65": {
       AOV: {
-        "bsa indexed": { mean: 12.1, sd: 1.6 },
-        "height indexed": { mean: 12.3, sd: 1.4 },
+        "bsa indexed": { mean: 12.1, sd: 1.6, xAxis: bsaArray },
+        "height indexed": { mean: 12.3, sd: 1.4, xAxis: htArray },
       },
       SOV: {
-        "bsa indexed": { mean: 19.3, sd: 2.8 },
-        "height indexed": { mean: 19.7, sd: 2.1 },
+        "bsa indexed": { mean: 19.3, sd: 2.8, xAxis: bsaArray },
+        "height indexed": { mean: 19.7, sd: 2.1, xAxis: htArray },
       },
       STJ: {
-        "bsa indexed": { mean: 16.6, sd: 2.6 },
-        "height indexed": { mean: 17.0, sd: 2.0 },
+        "bsa indexed": { mean: 16.6, sd: 2.6, xAxis: bsaArray },
+        "height indexed": { mean: 17.0, sd: 2.0, xAxis: htArray },
       },
     },
   },
 };
 
-// Function to generate chart data
-function generateChartData(model, sex, index) {
-  const data = models[model][index];
-  const xValues = data["x-axis"];
-  const sexData = data[sex];
-  const mean = sexData[0];
-  const sd = sexData[1];
-
+// Function to generate generic chart data
+function generateGenericChart(site, index) {
+  index = indexType[index];
+  const maleData = models.male.generic[site][index];
+  const femaleData = models.female.generic[site][index];
+  //console.log(`male data: ${maleData}, female data: ${femaleData}`);
   // Generate chart data based on the formula (mean + 2 * sd) * x
-  const chartData = xValues.map((x) => {
-    return [x, (mean + 2 * sd) * x];
+  maleData.chartData = maleData.xAxis.map((x) => {
+    switch (index) {
+      case "bsa indexed":
+        return [x, (maleData.mean + 2 * maleData.sd) * x];
+        break;
+      case "height indexed":
+        return [x, (maleData.mean + 2 * maleData.sd) * x / 100];
+        break;
+      case "height^2.13 indexed":
+        return [x, (maleData.mean + 2 * maleData.sd) * Math.pow(x/100, 2.13)];
+        break;
+    }
+  });
+  femaleData.chartData = femaleData.xAxis.map((x) => {
+    switch (index) {
+      case "bsa indexed":
+        return [x, (femaleData.mean + 2 * femaleData.sd) * x];
+        break;
+      case "height indexed":
+        return [x, (femaleData.mean + 2 * femaleData.sd) * x / 100];
+        break;
+      case "height^2.13 indexed":
+        return [x, (femaleData.mean + 2 * femaleData.sd) * Math.pow(x/100, 2.13)];
+        break;
+    }
   });
 
-  return chartData;
+  const chartData = {
+    labels: maleData.xAxis, // could be either male or female
+
+    datasets: [
+      {
+        label: "Male",
+        data: maleData.chartData,
+        borderColor: "blue",
+        backgroundColor: "rgba(0, 0, 255, 0.2)", // Optional: Set background color
+        // You can customize other dataset properties here
+      },
+      {
+        label: "Female",
+        data: femaleData.chartData,
+        borderColor: "salmon", // Change border color
+        backgroundColor: "rgba(250, 128, 114, 0.2)", // Change background color
+        // You can customize other dataset properties here
+      },
+    ],
+  };
+  // JS - Destroy exiting Chart Instance to reuse <canvas> element
+  let chartStatus = Chart.getChart("myChart"); // <canvas> id
+  if (chartStatus != undefined) {
+    chartStatus.destroy();
+  }
+  //-- End of chart destroy
+
+  const ctx = document.getElementById("myChart").getContext("2d");
+  const myChart = new Chart(ctx, {
+    type: "line",
+    data: chartData,
+    options: {
+      // You can customize chart options here
+    },
+  });
 }
-const maleData = generateChartData("sov", "male", "bsa");
-const femaleData = generateChartData("sov", "female", "bsa");
-
-const chartData = {
-  labels: bsaArray,
-
-  datasets: [
-    {
-      label: "Male",
-      data: maleData,
-      borderColor: "blue",
-      backgroundColor: "rgba(0, 0, 255, 0.2)", // Optional: Set background color
-      // You can customize other dataset properties here
-    },
-    {
-      label: "Female",
-      data: femaleData,
-      borderColor: "red",
-      backgroundColor: "rgba(255, 0, 0, 0.2)", // Optional: Set background color
-      // You can customize other dataset properties here
-    },
-  ],
-};
-
-// Example chart configuration using Chart.js
-const ctx = document.getElementById("myChart").getContext("2d");
-const myChart = new Chart(ctx, {
-  type: "line",
-  data: chartData,
-  options: {
-    // You can customize chart options here
-  },
-});
--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 $(document).ready(function () {
-
   // Define the generatePlotData function
   function generatePlotData() {
-    // get the visualization and site info
-    var visualization = $('#visualizationSelect').val();
-    var site = $('#site').val();
-    console.log(visualization, site);
-    
+    try {
+      // get the visualization and site info
+      var visualization = $("#visualizationSelect").val();
+      var siteAndIndex = $("#site").val();
+      //console.log(visualization, site);
+      var generic, gender, detail, site, index;
+      switch (visualization) {
+        case "generic":
+          generic = true;
+          break;
+        default:
+          generic = false;
+          detail = visualization.split(".")[0];
+          gender = visualization.split(".")[1];
+          break
+      }
+      if(siteAndIndex){ //it is possible that the choice landed on allometric indexing but is not available for race/age detail
+        site = siteAndIndex.split(".")[0];
+        index = siteAndIndex.split(".")[1];
+      };
+      //console.log(generic, detail, gender, site, index);
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+    // handle the generic male vs female charting
+    if(generic){
+      // just need the site & index
+      generateGenericChart(site, index);
 
+    }
   }
 
-
   // Function to update site select options based on visualization selection
-  $("#visualizationSelect").change(function() {
+  $("#visualizationSelect").change(function () {
     var selectedVisualization = $(this).val();
     var siteSelect = $("#site");
 
@@ -24,15 +46,12 @@ $(document).ready(function () {
       siteSelect.find("option[value$='.ht213']").prop("disabled", true);
     }
     generatePlotData();
-
   });
 
-  $('#site').change(function() {
+  $("#site").change(function () {
     generatePlotData();
   });
 
   // Trigger change event initially to set initial state
   $("#visualizationSelect").change();
-
-
 });
